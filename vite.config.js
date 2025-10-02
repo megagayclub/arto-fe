9cb12cba898d29b299f375d3f9de615faeb0bfae
sasimiseo <tjdbwns02@gmail.com>
2025-10-02 14:34:50 +0900

    Docker add

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  server: {
+    host: '0.0.0.0', // 컨테이너 외부에서 접근 가능하도록 설정
+    port: 3000,      // (선택 사항) 명시적으로 포트 지정
+  }
 })
