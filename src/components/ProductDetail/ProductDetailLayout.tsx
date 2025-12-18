7bb387d91493e2e4166876ee7727443ef52b1b4d
sewoo-hub <80295333+sewoo-hub@users.noreply.github.com>
2025-12-18 15:53:50 +0900

    ProductDetailType 제작년 장르 소재 액자  삭제

--- src/components/ProductDetail/ProductDetailLayout.tsx
+++ src/components/ProductDetail/ProductDetailLayout.tsx
@@ -23,10 +23,6 @@ const ProductInfoTable: React.FC<{ data: ProductDetailType }> = ({ data }) => {
   const infoRows = [
     { label: "작품명 | Title", value: data.title },
     { label: "작가명 | Artist", value: data.artist },
-    { label: "제작년 | Year", value: data.year },
-    { label: "장르 | Genre", value: data.genre },
-    { label: "소재 | Medium", value: data.medium },
-    { label: "액자 | Frame", value: data.frame },
     { label: "사이즈 | Size", value: data.size },
     { label: "배송비 | Shipping Cost", value: `${data.shippingCost.toLocaleString()}₩` },
     { label: "배송방법 | Shipping", value: data.shippingMethod },
